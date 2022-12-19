04c02da053f2032c0d49b97979c70bef73119466
Gerard Ruiz <102018322+GerardRuiz002@users.noreply.github.com>
2022-12-19 21:03:57 +0100

    AA

--- tests/defaultPackage/CartTest.java
+++
@@ -1,6 +0,0 @@
-package defaultPackage;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features/cart.feature", glue="steps")
-public class CartTest extends AbstractTestNGCucumberTests { }
--- tests/defaultPackage/ReviewOfProductTest.java
+++
@@ -1,6 +0,0 @@
-package defaultPackage;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features/reviewOfProduct.feature", glue="steps")
-public class ReviewOfProductTest extends AbstractTestNGCucumberTests { }
